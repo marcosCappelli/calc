9a66132e37661fca972872b506f86034a413e3ad
Marcoscappelli <cappelli.m@gmail.com>
2022-05-20 12:00:33 -0300

    javaScript na prática criando operações

--- js/scripts.js
+++ js/scripts.js
@@ -9,30 +9,63 @@ class calculator {
         this.currentOperationText = currentOperationText /*valor que está sempre impresso na tela*/
         this.currentOperation = "" /*valor digitado no momento*/
     }
-/*Método*/
+
+// Lógica
+// Primeira ação add digit to calculator screen
+// Método
 addDigit(digit) {
+    //Chega se a operação já tem um ponto operador "."
+    if(digit === "." && this.currentOperationText.innerText.includes(".")) {
+        return;
+    }
 
+    // console.log(digit); (((Esse comando serve apenas para mostrar valores )))
+    this.currentOperation = digit //Ao clicar no digito se saberá qual a operação sendo realizada
+    this.updateScreen() //método resposável por atualizar a tela
+}
+
+// Método
+// Process all calculator operation
+processOperation(operation) {
+
+    //Obtem current and previous valkue
+    let operationValue;
+    const previous = +this.previusOperationText.innerText;
+    const current = +this.currentOperationText.innerText;
+
+    switch (operation) {
+        case "+":
+            operationValue = previous + current;
+            this.updateScreen(operationValue, operation, current, previous);
+            break;
+        default:
+            return;
+    }
+}
+
+
+// Método
+// Muda values of the calculator screen
+updateScreen(operationValue = null, operation = null, current = null, previous = null) {
+    //Propriedade
+    this.currentOperationText.innerText += this.currentOperation; //todos os números digitados irão aparecer na tela
 }
 
 }
-
-
 
 /*Instância*/
 const calc = new calculator(previusOperationText, currentOperationText);
 
-
-
-
-
+/*Método*/
 buttons.forEach((btn)  => {
     btn.addEventListener("click", (e) => {
         const value = e.target.innerText;
         /*Se valor for maior ou igual a zero e valor igual a ponto console = valor*/
         if (+value >= 0 || value === ".") { 
-            console.log(value);
+            calc.addDigit(value);
+            // console.log(value); (((esse linha foi substituida pela de cima))) 
         } else { /*senão valor op + valor*/
-            console.log("op: " + value)
+            calc.processOperation(value);
         }
     });
 });
